92238228e226d4a9bfc7279b5e6a6fcdebeb42d7
daiict <shethyash222@gmail.com>
2022-05-17 13:49:45 +0530

    add few changes

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ import requests
 
 def index(request):
     return render(request, 'index.html')
+
+def nodes(request):
+    return render(request, 'nodes.html')
+
+def node(request, node_id):
+    return render(request, 'node.html')
